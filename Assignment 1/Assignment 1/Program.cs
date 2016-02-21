adbc5fc1df322f156b03419667789d4f3c48fa38
ADNAN RIBIC <adnanribic@ADNANs-MacBook-Pro.local>
2016-02-21 22:52:31 +0100

    Uploading first module

---
+++ Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string birthDate;
+            string addressLineOne;
+            string addressLineTwo;
+            string city;
+            string stateProvince;
+            int zipPostal;
+            string country;
+
+            firstName = "Adnan";
+            lastName = "Ribic";
+            birthDate = "31/01/1989";
+            addressLineOne = "Turbinveien 20";
+            addressLineTwo = "Prve Zenicke Brigade 15d";
+            city = "Oslo";
+            stateProvince = "Oslo area";
+            zipPostal = 195;
+            country = "Norway";
+
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(birthDate);
+            Console.WriteLine(addressLineOne);
+            Console.WriteLine(addressLineTwo);
+            Console.WriteLine(city);
+            Console.WriteLine(stateProvince);
+            Console.WriteLine(zipPostal);
+            Console.WriteLine(country);
+
+
+        }
+    }
+}
